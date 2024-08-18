a70abdc7fe7de3410214bde890127286e8df2883
Ankan <ankanganguly24@gmail.com>
2024-08-18 16:12:53 +0530

    components added, signup logic and schema done

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,7 +1,25 @@
+import SignUpFormProvider from "@/components/forms/sign-up/form-provider";
+
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex-1 py-36 md:px-16 w-full">
+      <div className="flex flex-col h-full gap-3">
+        <SignUpFormProvider>
+          <div>
+            <h1 className="text-3xl font-bold">Sign Up</h1>
+            <p className="text-gray-500">
+              Already have an account?{" "}
+              <a href="/auth/sign-in" className="text-blue-500">
+                Sign In
+              </a>
+            </p>
+          </div>
+        </SignUpFormProvider>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
